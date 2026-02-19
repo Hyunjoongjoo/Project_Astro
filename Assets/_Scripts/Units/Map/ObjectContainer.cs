600d44b6640b022246b0110b24382ad2fd4d4162
done778 <57587831+done778@users.noreply.github.com>
2026-02-19 12:53:47 +0900

    [fix] 브릿지 인덱스 추가

--- Assets/_Scripts/Units/Map/ObjectContainer.cs
+++ Assets/_Scripts/Units/Map/ObjectContainer.cs
@@ -4,4 +4,11 @@ public class ObjectContainer : Singleton<ObjectContainer>
 {
     public UnitBase[] blueSideStructure;
     public UnitBase[] redSideStructure;
+
+    public int BridgeIndex {  get; private set; }
+
+    private void Start()
+    {
+        BridgeIndex = 2;
+    }
 }
