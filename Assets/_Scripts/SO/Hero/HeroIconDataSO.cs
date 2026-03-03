ead768ea1acc3f288248674fc22371b768cfdb27
Dokkaeb <yj4705@gmail.com>
2026-03-03 14:33:00 +0900

    [feat] 스텟 아이콘 추가
    스텟아이콘SO 추가

---
+++ Assets/_Scripts/SO/Hero/HeroIconDataSO.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "HeroIconDataSO", menuName = "Scriptable Objects/HeroIconDataSO")]
+public class HeroIconDataSO : ScriptableObject
+{
+    [Serializable]
+    public struct HeroIconInfo
+    {
+        public string iconId; //테이블 매칭용
+        public Sprite iconSprite;
+    }
+
+    [SerializeField] private List<HeroIconInfo> _iconList = new List<HeroIconInfo>();
+    // 빠른 검색을 위한 딕셔너리 캐싱
+    private Dictionary<string, Sprite> _iconCache;
+
+    public Sprite GetIcon(string id)
+    {
+        if (_iconCache == null)
+        {
+            _iconCache = new Dictionary<string, Sprite>();
+            foreach (var info in _iconList)
+            {
+                if (!_iconCache.ContainsKey(info.iconId))
+                    _iconCache.Add(info.iconId, info.iconSprite);
+            }
+        }
+
+        if (_iconCache.TryGetValue(id, out Sprite sprite))
+        {
+            return sprite;
+        }
+
+        Debug.LogWarning($"[HeroIconDataSO] 아이콘 ID '{id}'를 찾을 수 없습니다.");
+        return null;
+    }
+
+}
