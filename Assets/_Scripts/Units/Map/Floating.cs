28395ac0ceb4e40fed7456c8109704255e6ef05d
Dokkaeb <yj4705@gmail.com>
2026-03-04 15:40:28 +0900

    [fix] 영웅목록 버튼 토글방식으로 변경, 스테이지 브릿지 플로팅 추가

---
+++ Assets/_Scripts/Units/Map/Floating.cs
@@ -0,0 +1,23 @@
+﻿using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    [SerializeField] List<Transform> _targets = new List<Transform>();
+    [SerializeField] float _moveRange = 30f;
+    [SerializeField] float _moveTime = 2f;
+    private void Start()
+    {
+        foreach (var target in _targets)
+        {
+            if (target == null) continue;
+
+            // 각 타겟의 현재 로컬 Y 위치를 기준으로 루프 생성
+            target.DOLocalMoveY(target.localPosition.y + _moveRange, _moveTime)
+                .SetEase(Ease.InOutSine)
+                .SetLoops(-1, LoopType.Yoyo)
+                .SetLink(target.gameObject);
+        }
+    }
+}
