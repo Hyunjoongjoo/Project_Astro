9074914c4b02e509086b959270e2e1e4d35dbd07
Dokkaeb <yj4705@gmail.com>
2026-03-10 09:58:19 +0900

    [feat] csv 테이블 최신화

--- Assets/_Scripts/TableData/UnitData.cs
+++ Assets/_Scripts/TableData/UnitData.cs
@@ -7,7 +7,7 @@ public class UnitData : ITableData
     public string unitName;
     public string unitDesc;
     public int unitType;
-    public int baseHp;//영웅데이터와 동일하게 인트로 변경
+    public int baseHp;
     public int baseAttackPower;
     public int baseHealingPower;
     public float attackSpeed;
