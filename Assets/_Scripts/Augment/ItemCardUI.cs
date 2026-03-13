df8c59f3c947fb3e006f90b50c472dbbe8fe4dc4
done778 <57587831+done778@users.noreply.github.com>
2026-03-13 17:12:19 +0900

    [feat] 아이템 카드 프리팹과 UI 스크립트 작성 중

---
+++ Assets/_Scripts/Augment/ItemCardUI.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCardUI : MonoBehaviour, IAugmentUI
+{
+    [Header("Main UI")]
+    [SerializeField] private Image _skillIconImg;      
+    [SerializeField] private TMP_Text _titleTxt;
+
+    [Header("Item Info UI")]
+    [SerializeField] private TMP_Text _itemDescription;
+
+    [Header("Button")]
+    [SerializeField] private Button _selectBtn;
+    [SerializeField] private GameObject _highlightObj;
+
+    private AugmentData _data;
+
+    public void Setup(AugmentData data)
+    {
+        _data = data;
+        _selectBtn.interactable = true;
+
+        // 아이콘 세팅
+        if (_skillIconImg != null) _skillIconImg.sprite = data.mainIcon;
+
+        //3. 증강 이름 세팅 (테이블에서 꺼내온 이름)
+        if (_titleTxt != null) _titleTxt.text = data.titleName;
+
+        //버튼 클릭 이벤트 세팅
+        _selectBtn.onClick.RemoveAllListeners();
+        _selectBtn.onClick.AddListener(OnSelectClicked);
+    }
+
+    public void ToggleHighlight(bool isOn)
+    {
+        if (_highlightObj != null) _highlightObj.SetActive(isOn);
+    }
+
+    private void OnSelectClicked()
+    {
+        GetComponentInParent<AugmentWindowUI>().OnCardSelected(this, _data);
+    }
+}
