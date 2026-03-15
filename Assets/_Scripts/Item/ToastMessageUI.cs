dfc9ff64088a03efb1bbdae32371305a0cec59c0
firedHouse <y.hyungu4704@gmail.com>
2026-03-15 16:57:17 +0900

    [feat] ToastMessageUI 구현
    아이템 토스트 메시지 전달용 UI 제작

---
+++ Assets/_Scripts/Item/ToastMessageUI.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+//일단 아이템 토스트메시지 전달용 UI
+public class ToastMessageUI : BaseUI
+{
+    public static ToastMessageUI Instance { get; private set; }
+
+    [Header("Toast Components")]
+    [SerializeField] private TextMeshProUGUI _toastText;
+
+    [Header("Settings")]
+    [SerializeField] private float _displayTime = 2.0f;
+
+    private Coroutine _toastCoroutine;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (Instance == null)
+        {
+            Instance = this;
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ShowToast(string message)
+    {
+        if (_toastText == null) return;
+
+        _toastText.text = message;
+
+        if (_toastCoroutine != null)
+        {
+            StopCoroutine(_toastCoroutine);
+        }
+
+        Open();
+
+        _toastCoroutine = StartCoroutine(ToastRoutine());
+    }
+
+    private IEnumerator ToastRoutine()
+    {
+        yield return CoroutineManager.waitForSeconds(_displayTime);
+
+        DeActivate();
+
+        _toastCoroutine = null;
+    }
+}
