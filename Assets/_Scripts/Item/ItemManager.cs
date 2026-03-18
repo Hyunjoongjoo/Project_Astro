9eb65d41699ea79a2473ee9fe73bfbd04625697d
firedHouse <y.hyungu4704@gmail.com>
2026-03-18 11:54:03 +0900

    [feat] 아이템 매니저 RPC 전달 대기 코루틴 로직 추가

--- Assets/_Scripts/Item/ItemManager.cs
+++ Assets/_Scripts/Item/ItemManager.cs
@@ -1,4 +1,5 @@
 using Fusion;
+using System.Collections;
 using UnityEngine;
 
 //아이템 장착, 공유 등의 가능여부를 서버가 검증하고 처리하기 위한 클래스
@@ -166,6 +167,13 @@ public class ItemManager : NetworkBehaviour
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     public void RPC_RefreshItemUI(PlayerRef targetPlayer)
     {
+        StartCoroutine(DelayedRefreshItemUI(targetPlayer));
+
+    }
+
+    private IEnumerator DelayedRefreshItemUI(PlayerRef targetPlayer)
+    {
+        yield return new WaitForSeconds(0.1f);
         //대상자 로컬에서만 새로고침
         if (Runner.LocalPlayer == targetPlayer)
         {
@@ -184,7 +192,10 @@ public class ItemManager : NetworkBehaviour
                 _stageManager.UpdateTeammateUI(targetPlayer, targetData.OwnedHeroes);
             }
         }
+
     }
+
+
 
     private void OnDestroy()
     {
