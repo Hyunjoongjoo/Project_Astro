49ae9ac26b1f48c128ac49e902b95ceb2bf3b56c
Dokkaeb <yj4705@gmail.com>
2026-03-25 17:40:01 +0900

    [fix] 베이스UI 사운드설정 추가,ui들사운드 설정 추가
    true,false로 사운드 재생여부 결정가능
    각종 ui들에사운드 재생여부 설정
    soundsetbtn 으로 사운드 선택가능하게 설계

--- Assets/_Scripts/UI/Macro/ChatManager.cs
+++ Assets/_Scripts/UI/Macro/ChatManager.cs
@@ -81,8 +81,8 @@ public class ChatManager : NetworkBehaviour
         }
         else
         {
-            _emoticonPanel.DeActivate(); // 반대쪽 끄기
-            _txtPanel.Open();       // 이쪽 켜기
+            _emoticonPanel.DeActivate(false); // 반대쪽 끄기
+            _txtPanel.Open(true);       // 이쪽 켜기
         }
     }
 
@@ -105,8 +105,8 @@ public class ChatManager : NetworkBehaviour
         }
         else
         {
-            _txtPanel.DeActivate(); // 반대쪽 끄기
-            _emoticonPanel.Open();       // 이쪽 켜기
+            _txtPanel.DeActivate(false); // 반대쪽 끄기
+            _emoticonPanel.Open(true);       // 이쪽 켜기
         }
     }
 
@@ -167,7 +167,7 @@ public class ChatManager : NetworkBehaviour
             SendChat(data.id);
 
             var panelUI = btn.GetComponentInParent<AnimUI>();
-            panelUI.DeActivate();
+            panelUI.DeActivate(false);
         });
     }
 
@@ -250,6 +250,8 @@ public class ChatManager : NetworkBehaviour
 
             // 내용 적용
             bubble.GetComponent<ChatBubbleUI>().Setup(macro);
+
+            AudioManager.Instance.PlayUISfx(UISfxList.Ping);
 
             // 2초 후 삭제
             Destroy(bubble, 2.0f);
