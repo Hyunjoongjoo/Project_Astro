b4328158d3f5f68a1562cb99f3f62be7fe16265e
firedHouse <y.hyungu4704@gmail.com>
2026-03-26 14:41:41 +0900

    [feat] 타이틀씬 패널 비활성화

--- Assets/_Scripts/UI/Augment/AugmentWindowUI.cs
+++ Assets/_Scripts/UI/Augment/AugmentWindowUI.cs
@@ -186,6 +186,7 @@ public class AugmentWindowUI : BaseUI
             {
                 int displayTime = Mathf.Max(0, Mathf.CeilToInt(timeLeft));
                 //콘피그 테이블 생기면 이 포맷 하드코딩 한 거 없애야함!(3.24 여현구)
+                //ingame_limit_time
                 _timerTxt.text = $"제한 시간: {displayTime}초";
             }
         }
