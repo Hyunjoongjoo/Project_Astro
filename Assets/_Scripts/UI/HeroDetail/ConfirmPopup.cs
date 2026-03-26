63e7d70266ae3cb43e3ab4d1bea199186e655c3a
Dokkaeb <yj4705@gmail.com>
2026-03-26 11:05:29 +0900

    [fix] 컨펌팝업 범용성있게 변경

--- Assets/_Scripts/UI/HeroDetail/ConfirmPopup.cs
+++ Assets/_Scripts/UI/HeroDetail/ConfirmPopup.cs
@@ -10,9 +10,9 @@ public class ConfirmPopup : BaseUI
     [SerializeField] private TMP_Text _yesBtnTxt;
     [SerializeField] private Button _noBtn;
 
-    public void Setup(string msg,Action onYes, bool canConfirm = true, string denyMsg = "")
+    public void Setup(string msg,Action onYes, bool canConfirm = true, string denyMsg = "", string yesText = "확인", string noText = "취소")
     {
-        //메시지 설정 돈충분 or 돈부족
+        //메시지 설정 조건따라 일반 또는 거절
         _messageTxt.text = canConfirm ? msg : denyMsg;
 
         // 돈부족하면 버튼 못누르게
@@ -21,9 +21,12 @@ public class ConfirmPopup : BaseUI
         // 버튼 텍스트 변경 
         if (_yesBtnTxt != null)
         {
-            _yesBtnTxt.text = canConfirm ? "확인" : "골드 부족";
+            _yesBtnTxt.text = yesText;
             _yesBtnTxt.color = canConfirm ? Color.white : Color.gray;
         }
+
+        //취소 버튼 텍스트도 바꾸고 싶으면 추가
+        // _noBtnTxt.text = noText;
 
         // YES 버튼 리스너
         _yesBtn.onClick.RemoveAllListeners();
